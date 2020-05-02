7399aa03faf1619ca7d345304c57c0b14b8e3599
Basko-Vladimir <1989bvg@gmail.com>
2020-05-02 14:08:20 +0300

    Updates

---
+++ precache-manifest.78ca3b1faf06e147be7f1c9f0dae0be8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fae643125e1bca06760577915b81c4f8",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "fbac2ff887a80fa74eef",
+    "url": "/Counter/static/css/main.5c3061c9.chunk.css"
+  },
+  {
+    "revision": "aac382f1f8c52f37f1ac",
+    "url": "/Counter/static/js/2.09729a22.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Counter/static/js/2.09729a22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbac2ff887a80fa74eef",
+    "url": "/Counter/static/js/main.7ea56488.chunk.js"
+  },
+  {
+    "revision": "d318f9cb2de8a854a11c",
+    "url": "/Counter/static/js/runtime-main.39c11da1.js"
+  }
+]);
